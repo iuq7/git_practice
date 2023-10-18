9069507327b658b78769ef75eaf34e8ae0cb3ddc
Irfan ul Haq Qureshi <irfanqureshi@cs.qau.edu.pk>
2023-10-19 00:13:54 +0500

    fix: merge conflicts resolved

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 console.log("Server is running on port 3030")
 <<<<<<< HEAD
-console.log("feature A. change in feature A from master branch")
 =======
 console.log("feature A, some more changing in the feature A")
 >>>>>>> featureA
